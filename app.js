3f3000c26b55a282f875fc75fba39a104d231542
Sabah Syed <sabisyed@gmail.com>
2020-04-03 20:18:52 -0700

    Working day planner SUCCESS

--- app.js
+++ app.js
@@ -16,23 +16,26 @@ $("#2pm").val(localStorage.getItem("2pm"));
 $("#3pm").val(localStorage.getItem("3pm"));
 $("#4pm").val(localStorage.getItem("4pm"));
 $("#5pm").val(localStorage.getItem("5pm"));
-var $time  = moment();
-var $currentTime = $("#currentDay").text($time.format("h:mm:ss a M/DD/YYYY"));
+//var startTime = "09:00";
+var $time = moment().format("H");
+var $currentDayTime = $("#currentDay").text(moment().format('LLLL'));
 var $saveBtn = $(".saveBtn");
 var $currentHour = $(".hour");
-var $nineAM = moment($currentHour.get(0).innerHTML , 'h:mma');
-var $tenAM = moment($currentHour.get(1).innerHTML , 'h:mma');
-var $elevenAM = moment($currentHour.get(2).innerHTML , 'h:mma');
-var $twelvePM = moment($currentHour.get(3).innerHTML , 'h:mma');
-var $onePM = moment($currentHour.get(4).innerHTML , 'h:mma');
-var $twoPM = moment($currentHour.get(5).innerHTML , 'h:mma');
-var $threePM = moment($currentHour.get(6).innerHTML , 'h:mma');
-var $fourPM = moment($currentHour.get(7).innerHTML , 'h:mma');
-var $fivePM = moment($currentHour.get(8).innerHTML ,'h:mma');
-var $current = moment().format("hA");
-var $nine = $nineAM.format("hA");
-console.log("$nine is :" + $nine);// output in 9am
-console.log("Current time is : " + $current); //output is 4pm
+//var $nineAM = moment($currentHour.get(0).innerHTML , 'H:mma');
+
+// Converting time to military to compare
+var $currentTime = moment().add( 21, 'H').hours();
+var $9am = moment($currentHour.get(0).innerHTML , 'H').format("H");
+var $10am =  moment($currentHour.get(1).innerHTML , 'H:mma').format("H");
+var $11am = moment($currentHour.get(2).innerHTML , 'H:mma').format("H");
+var $12pm = moment($currentHour.get(3).innerHTML , 'H:mma').format("H");
+var $1pm = moment($currentHour.get(4).innerHTML , 'H:mma').format("H");
+var $2pm = moment($currentHour.get(5).innerHTML , 'H:mma').format("H");
+var $3pm = moment($currentHour.get(6).innerHTML , 'H:mma').format("H");
+var $4pm =  moment($currentHour.get(7).innerHTML , 'H:mma').format("H");
+var $5pm = moment($currentHour.get(8).innerHTML ,'H:mma').format("H");
+
+
 $saveBtn.on("click" , function(){
     console.log("Save() started");
     // var isafter = moment().isAfter($fivePM);
@@ -48,7 +51,8 @@ $saveBtn.on("click" , function(){
     localStorage.setItem("5pm", $("#5pm").val());
     // if(($currentHour.get(0).innerHTML) <= time.format(h)){
     //         console.log("hello");
-   
+   console.log("I am edited  time: " + $currentTime);
+   console.log("I am local time: "+ $time);
    
     // console.log($currentHour.get(0).innerHTML);
 //    var noon  = moment($currentHour.get(3).innerHTML, 'h:mma');
@@ -56,8 +60,112 @@ $saveBtn.on("click" , function(){
 //     var endTime = moment('10:00am', 'h:mma');
 //     console.log(beginningTime.isBefore(endTime)); 
 //     console.log(noon.isBefore(endTime)); 
+   
+   if($1pm < $4pm ){
+       console.log($9am);
+   }
+   if($currentTime == $9am){
+    $("#9am").removeClass("past future");
+    $("#10am").removeClass(" present past ");
+    $("#11am").removeClass(" present past ");
+    $("#12pm").removeClass(" present past ");
+    $("#1pm").removeClass(" present past ");
+    $("#2pm").removeClass(" present past ");
+    $("#3pm").removeClass(" present past ");
+    $("#4pm").removeClass(" present past ");
+    $("#5pm").removeClass(" present past ");
+    }else
+   if($currentTime == $10am){
+    $("#9am").removeClass(" present future "); //past
+    $("#10am").removeClass(" past future");//present
+    $("#11am").removeClass(" present past "); //future
+    $("#12pm").removeClass(" present past ");
+    $("#1pm").removeClass(" present past ");
+    $("#2pm").removeClass(" present past ");
+    $("#3pm").removeClass(" present past ");
+    $("#4pm").removeClass(" present past ");
+    $("#5pm").removeClass(" present past ");
+   }else
+   if($currentTime == $11am){
+    $("#9am").removeClass(" present future ");
+    $("#10am").removeClass(" present future ");
+    $("#11am").removeClass(" past future");
+    $("#12pm").removeClass(" present past ");
+    $("#1pm").removeClass(" present past ");
+    $("#2pm").removeClass(" present past ");
+    $("#3pm").removeClass(" present past ");
+    $("#4pm").removeClass(" present past ");
+    $("#5pm").removeClass(" present past ");
+   }else
+   if($currentTime == $12pm){
+    $("#9am").removeClass(" present future ");
+    $("#10am").removeClass(" present future ");
+    $("#11am").removeClass(" present future ");
+    $("#12pm").removeClass(" past future");
+    $("#1pm").removeClass(" present past ");
+    $("#2pm").removeClass(" present past ");
+    $("#3pm").removeClass(" present past ");
+    $("#4pm").removeClass(" present past ");
+    $("#5pm").removeClass(" present past ");
 
-    
+   }else
+   if($currentTime == $1pm){
+    $("#9am").removeClass(" present future ");
+    $("#10am").removeClass(" present future ");
+    $("#11am").removeClass(" present future ");
+    $("#12pm").removeClass(" present future ");
+    $("#1pm").removeClass(" past future");
+    $("#2pm").removeClass(" present past ");
+    $("#3pm").removeClass(" present past ");
+    $("#4pm").removeClass(" present past ");
+    $("#5pm").removeClass(" present past ");
+   }else
+   if($currentTime == $2pm){
+    $("#9am").removeClass(" present future ");
+    $("#10am").removeClass(" present future ");
+    $("#11am").removeClass(" present future ");
+    $("#12pm").removeClass(" present future ");
+    $("#1pm").removeClass(" present future ");
+    $("#2pm").removeClass(" past future");
+    $("#3pm").removeClass(" present past ");
+    $("#4pm").removeClass(" present past ");
+    $("#5pm").removeClass(" present past ");
+   }else
+   if($currentTime == $3pm){
+    $("#9am").removeClass(" present future ");
+    $("#10am").removeClass(" present future ");
+    $("#11am").removeClass(" present future ");
+    $("#12pm").removeClass(" present future ");
+    $("#1pm").removeClass(" present future ");
+    $("#2pm").removeClass(" present future ");
+    $("#3pm").removeClass(" past future");
+    $("#4pm").removeClass(" present past ");
+    $("#5pm").removeClass(" present past ");
+   }else
+   if($currentTime == $4pm){
+    $("#9am").removeClass(" present future ");
+    $("#10am").removeClass(" present future ");
+    $("#11am").removeClass(" present future ");
+    $("#12pm").removeClass(" present future ");
+    $("#1pm").removeClass(" present future ");
+    $("#2pm").removeClass(" present future ");
+    $("#3pm").removeClass(" present future ");
+    $("#4pm").removeClass(" past future");
+    $("#5pm").removeClass(" present past ");
+   }
+   else
+   if($currentTime == $5pm){
+    $("#9am").removeClass(" present future ");
+    $("#10am").removeClass(" present future ");
+    $("#11am").removeClass(" present future ");
+    $("#12pm").removeClass(" present future ");
+    $("#1pm").removeClass(" present future ");
+    $("#2pm").removeClass(" present future ");
+    $("#3pm").removeClass(" present future ");
+    $("#4pm").removeClass(" present future ");
+    $("#5pm").removeClass(" past future");
+   }
+   
 }); 
 });
 
